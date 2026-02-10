d4dd6faa7dc8d0059f33bbbd472c4b3c2b212d1c
kristof2005 <42502546+kristof2005@users.noreply.github.com>
2026-02-10 20:40:10 +0100

    review fix

--- backend/src/review/reviewController.ts
+++ backend/src/review/reviewController.ts
@@ -66,16 +66,16 @@ export async function insertReview(request: any, response: Response) {
     }
 
     let review:Review = new Review(request.body)
-     if (!review.i_id || !review.u_id || !review.flagged || !review.stars ) {
+     if (!review.i_id ||!review.stars ) {
         return response.status(400).send({error: "Missing data"})
     }
-    if (review.i_id == null || review.u_id == null || review.flagged == null || review.stars == null) {
+    if (review.i_id == null || review.stars == null) {
         return response.status(400).send({error: "Missing data"})
     }
     const connection = await mysql.createConnection(config.database)
     try {
         const [results] = await connection.query(
-            "insert into reviews values (null, ?, ?, ?, ?, ?)", [review.i_id, review.u_id, review.flagged, review.stars, review.comment]
+            "insert into reviews values (null, ?, ?, ?, ?, ?)", [review.i_id, request.user.id, false, review.stars, review.comment]
         ) as Array<any>
         if (results.affectedRows > 0) {
             response.status(201).send({message:"Created"})
